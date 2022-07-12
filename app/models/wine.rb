1813131281787cfb6b41f7e2321454447310bc1d
Cristobal Dominguez <cristobald@gmail.com>
2022-07-11 22:09:12 -0400

    Agrega validación Para el Porcentaje de Cepas

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,7 +2,18 @@ class Wine < ApplicationRecord
     has_many :blends
     has_many :strains, through: :blends
 
+    validate :check_percentage
+
     accepts_nested_attributes_for :blends,
                                   allow_destroy: true,
                                   reject_if: proc { |attributes| attributes[:percentage].to_i <= 0 }
+    def check_percentage
+        percentages = self.blends.map { |blend| blend.percentage }
+        total_percentage = percentages.sum
+
+        unless total_percentage == 100
+            errors.add(:blends, "Los ensablajes deben sumar 100%")
+        end
+    end
+
 end
